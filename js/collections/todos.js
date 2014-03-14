4842032bbf390b55a3766b73904eb4d0ee3491c0
Marcus Christie <marc.christie@gmail.com>
2014-03-14 09:41:50 -0400

    Finished up TodoList and still working on AppView

--- js/collections/todos.js
+++ js/collections/todos.js
@@ -14,5 +14,31 @@ var TodoList = Backbone.Collection.extend({
   // Save all of the todo items under the `"todos-backbone"` namespace.
   localStorage: new Backbone.LocalStorage('todos-backbone'),
 
-  // TODO: stopped here
+  completed: function() {
+    return this.filter( function( todo ) {
+      return todo.get('completed');
+    });
+  },
+
+  // Filter down the list to only todo items that are still not finished.
+  // TODO: question: how does 'without' work?
+  remaining: function() {
+    return this.without.apply( this, this.completed() );
+  },
+
+  // We keep the Todoes in sequential order, despite being saved by unordered
+  // GUID in the database. This generates the next order number for new items.
+  nextOrder: function() {
+    if ( !this.length ){
+      return 1;
+    }
+    return this.last().get('order') + 1;
+  },
+
+  // Todos are sorted by their original insertion order.
+  comparator: function( todo ){
+    return todo.get('order');
+  }
 });
+
+app.Todos = new TodoList();
